6795597f14e6e44e0a5ea47cb79415a7dde1539f
onomatope2580 <79963946+onomatope2580@users.noreply.github.com>
2021-05-10 00:27:48 +0900

    recrods_newビューの実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,9 +2,15 @@ class RecordsController < ApplicationController
   require 'date'
 
   def index
+    @room = Room.find(params[:room_id])
+    @sueyoshis = @room.sueyoshis.all
+    @tamuras = @room.tamuras.all
+    @hamadas = @room.hamadas.all
+    @yamaguchis = @room.yamaguchis.all
   end
 
   def new
+    @room = Room.find(params[:room_id])
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "rooms#index"
-  resources :rooms, only: [:index, :new, :create]
-  resources :records, only: [:index, :new, :create]
+  resources :rooms, only: [:index, :new, :create] do
+    resources :records, only: [:index, :new, :create]
+  end
 end
