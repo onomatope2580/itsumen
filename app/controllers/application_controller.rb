377b329e6330112aae19961215e83ef72c8741ae
onomatope2580 <79963946+onomatope2580@users.noreply.github.com>
2021-05-16 15:00:26 +0900

    ベーシック認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      binding.pry
+      username == ENV["ITSUMEN_USER"] && password == ENV["ITSUMEN_PASSWORD"]
+    end
+  end
 end
